df1242ed6fc4f38d866203b4f0018cd5bbb6c646
c6supper@hotmail.com <c6supper@hotmail.com>
2022-03-18 11:56:37 +0000

    update

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # qnx_profiler
 
-**mkdir build && cd build && cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/QNXToolchain.cmake ../**
+## **1.Set your CMAKE_PREFIX_PATH environment variable to the Qt 5 installation prefix. This is the recommended way.**
+## **2.mkdir build && cd build && cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/QNXToolchain.cmake ../**
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
+#include <QCoreApplication>
+
 int main(int argc, char *argv[])
 {
+  QScopedPointer<QCoreApplication> app(new QCoreApplication(argc, argv));
+
+  // if (qobject_cast<QApplication *>(app.data()))
+  // {
+  //   // start GUI version...
+  // }
+  // else
+  // {
+  //   // start non-GUI version...
+  // }
+
+  return app->exec();
 }
